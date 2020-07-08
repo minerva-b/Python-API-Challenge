64062172cb57aab67219ce45319b2531fcb07dee
minerva-b <64626133+minerva-b@users.noreply.github.com>
2020-07-08 14:30:08 -0700

    Some progress...

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "386eb9d6ac764333c5c2ea46e82f0928"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCqKuzGYKB8THFWTN78ONv4SNUHJecbFJk"
